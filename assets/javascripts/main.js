e3bc23e347f53a9e7739a74ca09194f20a306640
Gregor Martynus <gregor@martynus.net>
2012-11-15 03:40:51 +0100

    added toggling for questionaires

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,1 +1,23 @@
 // !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+function showQuestionaire(event) {
+  $('article.more').removeClass('more')
+
+  if (location.hash) {
+    $(location.hash).addClass('more')
+    window.setTimeout( function() {
+      window.scrollTo(0, $(location.hash).offset().top) 
+    }, 10)
+    
+  } else {
+    if (event.oldURL) {
+      var anchor = event.oldURL.split('#')[1]
+
+      if (anchor) {
+        window.scrollTo(0, $('#' + anchor).offset().top) 
+      }
+    }
+  }
+}
+window.onhashchange = showQuestionaire;
+showQuestionaire({})
